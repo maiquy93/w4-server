15f4b8bb2f6790b0f92ae11509922aa2df2e016c
maiquy93 <maiquy6693@gmail.com>
2022-10-11 23:33:07 +0700

    env

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 var axios = require("axios");
 var cors = require("cors");
 const app = express();
+require("dotenv").config();
 app.use(cors());
-const port = 8000;
+const port = process.env.port || 8000;
+const store = process.env.store;
+const token = process.env.token;
 
 app.get("/getproducts", (req, res) => {
   var data = JSON.stringify({
@@ -24,6 +27,7 @@ app.get("/getproducts", (req, res) => {
             priceRangeV2{
               minVariantPrice{
                 amount
+                currencyCode
               }
             }
         
@@ -37,9 +41,9 @@ app.get("/getproducts", (req, res) => {
 
   var config = {
     method: "post",
-    url: "https://training-quy-mv-store.myshopify.com/admin/api/2022-10/graphql.json",
+    url: `https://${store}/admin/api/2022-10/graphql.json`,
     headers: {
-      "X-Shopify-Access-Token": "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",
+      "X-Shopify-Access-Token": token,
       "Content-Type": "application/json",
       Cookie:
         "_landing_page=%2Fpassword; _orig_referrer=https%3A%2F%2Ftraining-quy-mv-store.myshopify.com%2F%2Fshopify%2FProduct%2F8108101239024; _shopify_y=69508a70-1344-4bdb-a6bb-84ebdae6b92d; _y=69508a70-1344-4bdb-a6bb-84ebdae6b92d; cart_currency=VND; localization=VN; secure_customer_sig=",
@@ -56,8 +60,62 @@ app.get("/getproducts", (req, res) => {
     });
 });
 
-app.get("/test", (req, res) => {
-  res.json("ok");
+app.get("/getcollections", (req, res) => {
+  var axios = require("axios");
+  var data = JSON.stringify({
+    query: `{
+collections(first:15){
+  edges{
+    node{
+      id
+      title
+      image{
+        url
+      }
+      products(first:5){
+        edges{
+          node{
+            id
+            title
+            images(first:1){
+              edges{
+                node{
+                  url
+                }
+              }
+            }
+            priceRangeV2{
+             minVariantPrice{
+                amount
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+}`,
+    variables: {},
+  });
+
+  var config = {
+    method: "post",
+    url: `https://${store}/admin/api/2022-10/graphql.json`,
+    headers: {
+      "X-Shopify-Access-Token": token,
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
+
+  axios(config)
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
 
 app.listen(port, () => {
